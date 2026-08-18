a40d52420a562afa53236fde1b6a4ef8853ac3f9
ayoubmouhib <ayoub666mouhib@gmail.com>
2026-08-18 15:40:56 +0100

    Initialize PostgreSQL when FastAPI starts.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 
 from backend.app.api.routes.prediction import router as prediction_router
-
+from backend.app.db.init_db import init_db
 
 app = FastAPI(
     title="Intelligent Intrusion Detection System",
     description="AI-powered network intrusion detection API",
     version="1.0.0",
 )
+
+@app.on_event("startup")
+def startup():
+
+    init_db()
 
 
 @app.get("/health")
